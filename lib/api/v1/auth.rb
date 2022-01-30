a86780ea7f14df777148c1084c23b1d83335342e
Janice He <>
2022-01-30 23:55:07 +0800

    improve: public api method

--- lib/api/v1/auth.rb
+++ lib/api/v1/auth.rb
@@ -1,4 +1,17 @@
 class API::V1::Auth < Grape::API
+
+  before do
+    # TODO: implement authentication
+    @current_user = User.find_by(id: headers['User-Id'])
+    raise_error(403, 'user not found') if @current_user.nil?
+  end
+
+  helpers do
+    def current_user
+      @current_user
+    end
+  end
+
   resource :auth do
     mount API::V1::Auth::Deposit
     mount API::V1::Auth::Withdraw
--- lib/api/v1/root.rb
+++ lib/api/v1/root.rb
@@ -1,5 +1,16 @@
 class API::V1::Root < Grape::API
   version  'v1'
+
+  helpers do
+    def raise_error(code, message)
+      error_hash = {
+        error_code: code,
+        error_message: message
+      }
+      error!(error_hash)
+    end
+  end
+
   mount API::V1::Ping
   mount API::V1::Auth
   mount API::V1::NonAuth
