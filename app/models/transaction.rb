fcbaab2f1bf8782eedb8b576c7ee21ad84d216f9
Janice He <>
2022-01-30 14:22:30 +0800

    feat: validate transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -28,4 +28,30 @@ class Transaction < ApplicationRecord
 
   enum action: { purchase: 0, refund: 1 }
   enum currency: Account.currencies
+
+  validate :valid_amount_price
+  validate :valid_account_and_currency
+  validate :enough_balance
+
+  private
+
+  def valid_amount_price
+    case action
+    when 'deposit'
+      errors.add(:amount, 'amount should greater than 0 since it is deposited') if amount <= 0
+    when 'withdraw'
+      errors.add(:amount, 'amount should less than 0 since it is withdrawed') if amount >= 0
+    end
+  end
+
+  def valid_account_and_currency
+    errors.add(:account_and_currency, 'Transfer to wrong account type') unless currency == account.currency
+  end
+
+  def enough_balance
+    return if amount > 0
+
+    cal_balance = account.balance + amount
+    errors.add(:balance, 'insufficient balance') if cal_balance < 0
+  end
 end
